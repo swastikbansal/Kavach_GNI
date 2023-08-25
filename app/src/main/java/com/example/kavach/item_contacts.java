e35ce9d990bb77babd199fb18bf0c9ca866e706a
Yatharth Patankar <yatharthpatankar07@gmail.com>
2023-08-25 15:03:40 +0530

    added the contact info file and item_contact java file

---
+++ app/src/main/java/com/example/kavach/item_contacts.java
@@ -0,0 +1,15 @@
+package com.example.kavach;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kavach.R;
+
+public class item_contacts extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_contact);
+    }
+}
